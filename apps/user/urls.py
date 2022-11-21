7c006f4791d8cdd51f5f300540f99b96f38eb252
Sherry <shao19980209@gmail.com>
2022-11-21 18:57:08 -0400

    class view

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import re_path
 from apps.user import views
+from apps.user.views import register_view
 
 urlpatterns = [
-    re_path(r'^register_handle', views.register_handle, name='register_handle'),  #
-    re_path(r'^register', views.register, name='register'),  #
+    re_path(r'^register', register_view.as_view(), name='register'),  #
 
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,5 +1,5 @@
 import re
-
+from django.views.generic import View
 from django.shortcuts import render, redirect
 from django.urls import reverse
 # from django.contrib.auth.models import User
@@ -12,11 +12,10 @@ User = get_user_model()
 
 def register(request):
     '''Access register page'''
-    return render(request, 'register.html')
+    if request.method == 'GET':
+        return render(request, 'register.html')
 
-
-def register_handle(request):
-    '''Process register'''
+    # Process register
     # accept data
     username = request.POST.get('user_name')
     password = request.POST.get('pwd')
@@ -26,17 +25,66 @@ def register_handle(request):
     # check data
     if not all([username, password, email]):
         # lack data
-
         return render(request, 'register.html', {'errmsg': 'Lack information'})
 
     if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
         return render(request, 'register.html', {'errmsg': 'Email Error'})
     if allow != 'on':
         return render(request, 'register.html', {'errmsg': 'Please allow agreement'})
-    print(123123)
+    # check id if exist
+    try:
+        User.objects.get(username=username)
+    except User.DoesNotExist:
+        user = None
+
+    if user:
+        return render(request, 'register.html', {'errmsg': 'The user exists'})
+
     # process:create account
     user = User.objects.create_user(username, email, password)
+    user.is_active = 0
     user.save()
-    print(456456)
     # response, jump to front page
     return redirect(reverse('goods:index'))
+
+
+class register_view(View):
+    '''REGISTER'''
+
+    def get(self, request):
+        '''Show Register'''
+        return render(request, 'register.html')
+
+    def post(self, request):
+        '''Process Register'''
+        # Process register
+        # accept data
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+
+        # check data
+        if not all([username, password, email]):
+            # lack data
+            return render(request, 'register.html', {'errmsg': 'Lack information'})
+
+        if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+            return render(request, 'register.html', {'errmsg': 'Email Error'})
+        if allow != 'on':
+            return render(request, 'register.html', {'errmsg': 'Please allow agreement'})
+        # check id if exist
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            user = None
+
+        if user:
+            return render(request, 'register.html', {'errmsg': 'The user exists'})
+
+        # process:create account
+        user = User.objects.create_user(username, email, password)
+        user.is_active = 0
+        user.save()
+        # response, jump to front page
+        return redirect(reverse('goods:index'))
